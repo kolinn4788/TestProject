4b2e29ebaa8cb3f4dfa8f5d5eff793afba65e333
kolinn <phonezawmyint@uit.edu.mm>
2016-08-18 17:29:43 +0630

    nine

--- src/test/kalama.java
+++ src/test/kalama.java
@@ -1,16 +1,6 @@
-﻿package test;
+package test;
 
 public class test {
 	public static void main(String args[]){
 		package test;
 
-public class test {
-	public static void main(String args[]){
-		system.out.print(“kalama a yuu”);
-	}
-
-}
-
-	}
-
-}
