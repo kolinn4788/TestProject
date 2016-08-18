850c2cad0258e9b50b932e4efcdf0180c0faf060
kolinn <phonezawmyint@uit.edu.mm>
2016-08-18 17:07:06 +0630

    fifth
    fifth

--- src/test/test.java
+++ src/test/test.java
@@ -1,9 +1,9 @@
-package test;
+﻿package test;
 
 public class test {
 	public static void main(String args[]){
 		system.out.print(“a you ma pont pont”);
-system.out.print(“a you ma pont pont”);system.out.print(“a you ma pont pont”);system.out.print(“a you ma pont pont”);
+system.out.print(“a you ma pont pont”);system.out.print(“a you lin lin”);system.out.print(“a you linn linn”);
 
 	}
 
