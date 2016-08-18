d0c8b2dc770e6a809b61fa5f40b8b7655db0259a
kolinn <phonezawmyint@uit.edu.mm>
2016-08-18 16:59:52 +0630

    forth

--- src/test/test.java
+++ src/test/test.java
@@ -2,7 +2,9 @@ package test;
 
 public class test {
 	public static void main(String args[]){
-		
+		system.out.print(“a you ma pont pont”);
+system.out.print(“a you ma pont pont”);system.out.print(“a you ma pont pont”);system.out.print(“a you ma pont pont”);
+
 	}
 
 }
